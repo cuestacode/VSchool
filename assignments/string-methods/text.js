8ebdde82dbb1376c8bccd81aa7ee839bba21e1a6
Quest Cuesta <cuestacode@gmail.com>
2023-02-27 13:11:05 -0500

    third commit

---
+++ assignments/string-methods/text.js
@@ -0,0 +1,6 @@
+// Write a function that takes a string as a parameter and returns the same string in all capital letters followed by all lowercase letters.
+var name = "MICHAEL"
+var lowercasedName = name.toLowerCase ()
+console.log(lowercasedName)
+
+//Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down.
