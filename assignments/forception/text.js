ae3000da36d1c945a60b3c7e1134f6fba32a2d59
Quest Cuesta <cuestacode@gmail.com>
2023-02-27 16:40:18 -0500

    fourth commit

--- assignments/forception/text.js
+++ assignments/forception/text.js
@@ -1,2 +1,16 @@
-var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
+const people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+function forception (people,alphabet){
+  let upperCaseAlphabet = alphabet.toUpperCase()
+
+  let splitUpperCaseAlphabet = upperCaseAlphabet.split("")
+  splitUpperCaseAlphabet.reverse()
+  for (var i = people.length - 1;i >= 0; i--){
+    for (var j = 0; j < splitUpperCaseAlphabet.length; j++){
+    people.splice([i + 1], 0, splitUpperCaseAlphabet[j])
+    }
+  }
+    console.log(people)
+}
+forception(people, alphabet)
--- assignments/string-methods/text.js
+++ assignments/string-methods/text.js
@@ -1,6 +1,13 @@
 // Write a function that takes a string as a parameter and returns the same string in all capital letters followed by all lowercase letters.
-var name = "MICHAEL"
-var lowercasedName = name.toLowerCase ()
-console.log(lowercasedName)
+var userName = "michael"
+
+
+var allLowerCase = "michael"
+function upperLower(name){
+  var upperCasedName = name.toUpperCase()
+  var lowerCasedName = name
+return upperCasedName.concat(lowerCasedName)
+}
+console.log(upperLower(userName))
 
 //Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down.
