4a0bf6572d8552cb51a5e5cfe5ef5b61164c2aac
Quest Cuesta <cuestacode@gmail.com>
2023-02-27 19:01:34 -0500

    fifth commit

--- assignments/string-methods/text.js
+++ assignments/string-methods/text.js
@@ -8,6 +8,25 @@ function upperLower(name){
   var lowerCasedName = name
 return upperCasedName.concat(lowerCasedName)
 }
-console.log(upperLower(userName))
+//console.log(upperLower(userName))
 
 //Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down.
+let ex2 = "Hello World"
+function example2 (ex2) {
+var getHalf = (Math.floor(ex2.length /2))
+
+console.log(getHalf)
+}
+example2(ex2)
+
+//Write a function that uses slice() and the other functions you've written to return the first half of the given string.
+//returnFirstHalf("Hello") // => "He"
+//returnFirstHalf("Hello World") // => "Hello"
+let ex3 = "Hello World"
+function example3(ex3){
+  let slicedEx3 = ex3.slice(0, 5)
+  console.log(slicedEx3)
+}
+example3(ex3)
+//Write a function that takes a string as a parameter and returns that string where the first half is capitalized, and the second half is lowercase.
+//> Hint: If your string length is odd, use Math.floor() to round down.
